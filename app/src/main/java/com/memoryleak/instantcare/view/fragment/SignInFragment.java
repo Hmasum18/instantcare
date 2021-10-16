a87f8446ced9496231253610875cc44238f7511f
Hmasum18 <1805052@ugrad.cse.buet.ac.bd>
2021-10-17 01:55:19 +0600

    designed sign up fragment

--- app/src/main/java/com/memoryleak/instantcare/view/fragment/SignInFragment.java
+++ app/src/main/java/com/memoryleak/instantcare/view/fragment/SignInFragment.java
@@ -2,7 +2,10 @@ package com.memoryleak.instantcare.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,5 +35,12 @@ public class SignInFragment extends Fragment {
         // Inflate the layout for this fragment
         mVB = FragmentSignInBinding.inflate(inflater, container, false);
         return mVB.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mVB.signUpBtn.setOnClickListener(v -> {
+            NavHostFragment.findNavController(this).navigate(R.id.signUpFragment);
+        });
     }
 }
